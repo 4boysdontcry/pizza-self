76c034f93abe4499e5fd4d9e90e4e3562d6d0c71
4boysdontcry <hgy02166@gmail.com>
2021-04-14 18:28:57 +0900

    monavi-end

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+/*************** 글로벌 설정 *****************/
 $('.monavi-wrap').slideUp(0)
 
-$('.monavi-bars').on('mouseenter', onNaviEnter)
 
-function onNaviEnter(){
-    $(this).find('.monavi-wrap').slideDown(300)
+/*************** 사용자 함수 *****************/
+
+
+/*************** 이벤트 등록 *****************/
+$('.monavi-bars').on('mouseover', onNaviOver)
+$('.header-wrapper').on('mouseleave', onNaviLeave)
+
+
+/*************** 이벤트 콜백 *****************/
+function onNaviOver(){
+    $('.header-wrapper').find('.monavi-wrap').slideDown(300)
+}
+
+function onNaviLeave(){
+    $(this).find('.monavi-wrap').slideUp(300)
 }
